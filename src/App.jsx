7f506574e07df7860d21b9335c1317fae31c50b5
Cyber7711 <sarvareliboyev8@gmail.com>
2026-03-30 17:56:39 +0500

    fix: JSX ichidagi xato izohlar olib tashlandi

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,18 @@
 import React from "react";
 import { HashRouter, Routes, Route } from "react-router-dom";
 
-// 1. Context Providerlarni chaqiramiz
 import { AppProvider } from "./context/AppContext";
 import { CartProvider } from "./context/CartContext";
 
-// 2. Sahifalarni chaqiramiz
 import MenuPage from "./pages/MenuPage";
-import CheckoutPage from "./pages/CheckOutPage";
+import CheckoutPage from "./pages/CheckoutPage";
 
 const App = () => {
   console.log("[APP] Dastur ishga tushmoqda... React DOM chizilmoqda.");
 
   return (
-    /* 1-qavat: Telegram va Do'kon ID sini (tenantId) butun dasturga tarqatadi */
     <AppProvider>
-      /* 2-qavat: Savat tizimi (Buyurtmalar xotirasi) */
       <CartProvider>
-        /* 3-qavat: Sahifalar o'rtasida xavfsiz harakatlanish */
         <HashRouter>
           <div
             className="app-container"
@@ -28,12 +23,8 @@ const App = () => {
               minHeight: "100vh",
             }}
           >
-            {/* Marshrutlar (Routes) ni belgilaymiz */}
             <Routes>
-              {/* Asosiy (bosh) sahifaga MenuPage ni bog'ladik */}
               <Route path="/" element={<MenuPage />} />
-
-              {/* Checkout sahifasi */}
               <Route path="/checkout" element={<CheckoutPage />} />
             </Routes>
           </div>
