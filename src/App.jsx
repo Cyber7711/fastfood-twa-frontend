e6853be83c2dd4e77cce29aa929e0a8028fc96f7
Cyber7711 <sarvareliboyev8@gmail.com>
2026-03-30 20:21:04 +0500

    feat: added modern skeleton loader for menu page

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import PageWrapper from "./components/PageWrapper"; // Yangi wrapper
 import MenuPage from "./pages/MenuPage";
-import CheckoutPage from "./pages/CheckoutPage";
+import CheckoutPage from "./pages/CheckOutPage";
 
 const App = () => {
   return (
